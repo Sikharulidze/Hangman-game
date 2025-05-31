1ba9c345fe56b89a8b5eed07a94d0131bb324a2a
Mariam Sikharulidze <mariam.sikha.22@gmail.com>
2025-05-31 20:53:07 +0400

    fixed structure of page

--- src/Rules.jsx
+++ src/Rules.jsx
@@ -28,7 +28,6 @@ function Rules({ setScreen }) {
           size={circleSize}
           onClick={() => setScreen("home")}
           className="back-button-circle"
-          
         >
           <img src="/images/icon-back.svg" alt="Back" className="back-icon" />
         </GradientCircle>
@@ -39,32 +38,38 @@ function Rules({ setScreen }) {
       <div className="rules-container">
         <div className="rule-box">
           <h1 className="rule-number1">01</h1>
-          <strong className="rule-title">CHOOSE A CATEGORY</strong>
-          <p className="rule-p">
-            First, choose a word category, like animals or movies. The computer
-            then randomly selects a secret word from that topic and shows you
-            blanks for each letter of the word.
-          </p>
+          <div className="rule-text">
+            <strong className="rule-title">CHOOSE A CATEGORY</strong>
+            <p className="rule-p">
+              First, choose a word category, like animals or movies. The
+              computer then randomly selects a secret word from that topic and
+              shows you blanks for each letter of the word.
+            </p>
+          </div>
         </div>
 
         <div className="rule-box">
           <h1 className="rule-number">02</h1>
-          <strong className="rule-title">GUESS LETTERS</strong>
-          <p className="rule-p">
-            Take turns guessing letters. The computer fills in the relevant
-            blank spaces if your guess is correct. If it's wrong, you lose some
-            health, which empties after eight incorrect guesses.
-          </p>
+          <div className="rule-text">
+            <strong className="rule-title">GUESS LETTERS</strong>
+            <p className="rule-p">
+              Take turns guessing letters. The computer fills in the relevant
+              blank spaces if your guess is correct. If it's wrong, you lose
+              some health, which empties after eight incorrect guesses.
+            </p>
+          </div>
         </div>
 
         <div className="rule-box">
           <h1 className="rule-number">03</h1>
-          <h2 className="rule-title">WIN OR LOSE</h2>
-          <p className="rule-p">
-            You win by guessing all the letters in the word before your health
-            runs out. If the health bar empties before you guess the word, you
-            lose.
-          </p>
+          <div className="rule-text">
+            <h2 className="rule-title">WIN OR LOSE</h2>
+            <p className="rule-p">
+              You win by guessing all the letters in the word before your health
+              runs out. If the health bar empties before you guess the word, you
+              lose.
+            </p>
+          </div>
         </div>
       </div>
     </div>
